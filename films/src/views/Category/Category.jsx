fea65bff050b37a242de9f4aceb4e4d2c4970cab
ABDUGANIYEV-01 <muhsimovrahimjon@gmail.com>
2022-03-26 10:13:17 +0600

    first commit

--- films/src/views/Category/Category.jsx
+++ films/src/views/Category/Category.jsx
@@ -13,3 +13,9 @@ function Catagory() {
 }
 
 export default Catagory;
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ABDUGANIYEV-01/5oy-imtixon.git
+git push -u origin main
