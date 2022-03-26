b38e02e913390f26a605785b283349152b60504b
ABDUGANIYEV-01 <muhsimovrahimjon@gmail.com>
2022-03-26 10:02:57 +0600

    first commmit

--- films/src/views/Category/Category.jsx
+++ films/src/views/Category/Category.jsx
@@ -1,47 +1,15 @@
-// import { fireEvent } from '@testing-library/react';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Error from '../../components/Error/Error';
+
+import React from 'react';
+import MyLastWatch from '../../components/myLastWatch/myLastWAtch';
 
 
 
-const Catagory = () => {
-    const [posts, setPosts] = useState([])
-
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState("")
-    const API = 'https://www.omdbapi.com/?s=Batman&page=5&apikey=8b0efe8d'
-    useEffect(() => {
-        axios({
-            url: `${API}`,
-            method:'GET',
-           
-            
-        
-        }) 
-        .then(res => { 
-            console.log([res.data]);           
-            setPosts([res.data])
-            setLoading(false)    
-            setError("")
-        }).catch(err => {
-           setError(err.message)
-        })
-    }, [])
-    return (
-        <div className='blog container'>
-            {error ? <Error error={error} /> :
-                loading ? "Loading....." :  
-                    posts.length === 0 ? "Posts not found" :
-                        posts.flatMap((element , index) => {
-                            return <div key={index}>
-                                {element?.Title}
-                                <p>{element?.Year}wefrg</p>
-                            </div>
-                        }) 
-            }
+function Catagory() {
+  
+    return  <div className='blog container'>  
+           <MyLastWatch/>
         </div>
-    );
+    ;
 }
 
 export default Catagory;
